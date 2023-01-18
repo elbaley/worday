0809c0590d1d40b746b112f8a17b1e2f5fc6e207
elbaley <lebafurkan@outlook.com>
2023-01-18 17:25:25 +0300

    add react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src='/vite.svg' className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://reactjs.org' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <h1>worday</h1>
+
+      <p className='read-the-docs'>hello world</p>
+    </div>
+  );
+}
+
+export default App;
