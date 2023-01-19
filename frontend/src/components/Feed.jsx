01a945d148ffcb7bdabfba16a1d01c4ac6785b8c
yemrecan <yildirimemrecan@outlook.com>
2023-01-19 16:43:58 +0300

    update profile menu

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -2,7 +2,7 @@ const Feed = () => {
   return (
     <div className=' border-x border-x-zinc-800  col-span-2 text-white '>
       <h2 className='text-4xl pl-5 pt-3'>feed </h2>
-      <div className='flex px-5 items-center gap-2 my-5 relative border-y border-y-zinc-800  py-3'>
+      <div className='flex px-5 items-center gap-3 my-5 relative border-y border-y-zinc-800  py-3'>
         <img
           className='rounded-full h-20 hover:opacity-80 '
           src='https://picsum.photos/300/300'
