8003e89dee92b7c9bcbdcadacd83b78c23859275
elbaley <lebafurkan@outlook.com>
2023-02-19 04:38:34 +0300

    add bg blur to profile page title

--- frontend/src/components/PageTitle.jsx
+++ frontend/src/components/PageTitle.jsx
@@ -4,7 +4,7 @@ import { AiOutlineArrowLeft, AiFillDelete } from "react-icons/ai";
 const PageTitle = ({ title, backButton, children }) => {
   const navigate = useNavigate();
   return (
-    <section className="flex items-center border-b border-y-zinc-800 ">
+    <section className="sticky top-0 z-10 flex items-center border-b border-y-zinc-800 bg-black bg-opacity-60 backdrop-blur-md ">
       {backButton && (
         <button
           className="ml-2 rounded-full p-2 hover:bg-zinc-500 hover:bg-opacity-60"
@@ -15,7 +15,7 @@ const PageTitle = ({ title, backButton, children }) => {
           <AiOutlineArrowLeft size={24} />
         </button>
       )}
-      <div className="sticky top-0 z-10  bg-black bg-opacity-60 py-2 pl-5 backdrop-blur-md	">
+      <div className="py-2 pl-5 ">
         <h2 className="pt-2 text-2xl font-bold">{title} </h2>
         {children}
       </div>
